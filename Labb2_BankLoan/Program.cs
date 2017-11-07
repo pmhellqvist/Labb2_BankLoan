fd7b1f53aa6ed93201057f8195ed6eaecca81939
Per <pmhellqvist@gmail.com>
2017-11-07 03:38:22 +0100

    Initierade variabler samt skrivit vad konsolen säga vid programstart.

--- Labb2_BankLoan/Program.cs
+++ Labb2_BankLoan/Program.cs
@@ -2,11 +2,24 @@
 
 namespace Labb2_BankLoan
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+
+        
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int years, loanAmount;
+            double totalAmount;
+
+            Console.WriteLine("Hej och välkommen till banken!");
+            Console.Write("\nÖnskat lånebelopp (kr): ");
+            loanAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nRäntesatser:\n1 år: 3%\n2 år: 4%\n3 år: 5%\n4 år: 6%\n5 år: 7%\n6 år: 8%\n7 år: 9%\n8 år: 10%\n9 år: 11%\n10 år: 12%");
+            Console.Write("\nÖnskad återbetalningstid (1-10 år): ");
+            years = int.Parse(Console.ReadLine());
+
+            
         }
     }
 }
