bb76db240b846d3aefe8173080de918f6724b157
Per <pmhellqvist@gmail.com>
2017-11-07 03:28:34 +0100

    Add project files.

---
+++ Labb2_BankLoan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb2_BankLoan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
